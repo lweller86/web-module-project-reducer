e4f6254198313547b92bab117e999d395d72a8ec
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-01-31 09:57:54 -0500

    one function added to # 1

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,9 @@ import './App.css';
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 import reducer, { initialState } from '../reducers';
+import { addOne } from '../actions/index';
+
+
 
 
 
@@ -12,7 +15,9 @@ import reducer, { initialState } from '../reducers';
 function App() {
   const [ state, dispatch ] = useReducer(reducer, initialState)
 
-
+const handleClick = () => {
+  dispatch( addOne() )
+}
 
   return (
     <div className="App">
@@ -37,7 +42,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={handleClick}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
